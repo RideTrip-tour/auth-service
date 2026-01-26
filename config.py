194dd389f843d4aed75c7a5ed8189ff847cb8305
Viktor Balonkin <v.balonkin@gmail.com>
2026-01-26 22:18:30 +0300

    docker

--- config.py
+++ config.py
@@ -1,23 +1,29 @@
+import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    jwt_secret: str = "secret"
-    jwt_algorithm: str = "HS256"
-    redis_url: str = "redis://127.0.0.1:6379"
+    jwt_secret: str = os.getenv("JWT_SECRET", "secret")
+    jwt_algorithm: str = os.getenv("JWT_ALGORITHM", "HS256")
+    
+    # Redis
+    redis_url: str = os.getenv("REDIS_URL", "redis://redis:6379")
     redis_ttl: int = 300  # время жизни кеша по умолчанию
-    db_name: str = "rtt"
-    db_host: str = "127.0.0.1"
-    db_port: int = 5432
-    db_user: str = "platform"
-    db_pass: str = "12345"
+    
+    # Database
+    db_host: str = os.getenv("DB_HOST", "postgres")
+    db_port: int = int(os.getenv("DB_PORT", "5432"))
+    db_name: str = os.getenv("DB_NAME", os.getenv("POSTGRES_DB", "mydb"))
+    db_user: str = os.getenv("DB_USER", os.getenv("POSTGRES_USER", "user"))
+    db_pass: str = os.getenv("DB_PASS", os.getenv("POSTGRES_PASSWORD", "password123"))
     db_driver: str = "postgresql+asyncpg"
+    
     access_token_expire_sec: int = 60 * 60 * 24 * 7
     google_oauth_client_id: str = ""
     google_oauth_client_secret: str = ""
 
     model_config = SettingsConfigDict(
-        env_file=".env", env_file_encoding="utf-8", env_ignore_empty=True
+        extra="ignore",
     )
 
 
