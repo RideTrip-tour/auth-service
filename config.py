acb4abad449a42c7bd4bdd9a6bc64731a0398fa6
Viktor Balonkin <v.balonkin@gmail.com>
2026-03-25 16:41:49 +0300

    skip warnings

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     # =========================
     # Security
     # =========================
-    jwt_secret: str = "secret"
+    jwt_secret: str = "secretsecretsecretsecretsecretsecret"
     jwt_algorithm: str = "HS256"
     gateway_name: str = "Gate"
     debug: bool = True
