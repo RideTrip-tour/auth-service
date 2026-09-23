2f3ab7e8b58659a7878436166aa9dc8d574e876a
Viktor Balonkin <v.balonkin@gmail.com>
2026-09-23 21:03:59 +0300

    https://ru.yougile.com/team/d193f0b54f05/#KAN-53

--- app/routes/reset_pass.py
+++ app/routes/reset_pass.py
@@ -50,16 +50,15 @@ def get_reset_password_router(
     ):
         try:
             user = await user_manager.get_by_email(email_forgot_pass.email)
-        except exceptions.UserNotExists as exc:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail="USER NOT FOUND",
-            ) from exc
+        except exceptions.UserNotExists:
+            return None
 
         try:
             await user_manager.forgot_password(user, request)
         except exceptions.UserInactive:
             pass
+
+        return None
 
     @router.post(
         "/reset-password",
